f02b05e8f6bf6c4a2620291f78f364d3be92ee1b
Kelvin Cuesta <kelvinscuesta@gmail.com>
2020-04-23 15:22:57 -0400

    Fixed the footer, navBar and font transitions to be responsive

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,15 +1,121 @@
 <template>
-  <div class="pa1">
-    <a href="https://www.linkedin.com/in/kelvinscuesta/"
-      ><img class="mw1 mr2" src="../static/linkedin.svg"
-    /></a>
+  <div class="flex justify-center">
     <a
-      href="https://docs.google.com/document/d/1gte1j7zx5yYry9QdiE02Cm_I6dRooGZHsTzmGD3u0lo/edit?usp=sharing"
-      ><img class="mw1 mr2" src="../static/resume-svgrepo-com.svg"
-    /></a>
-    <a href="https://github.com/kelvinscuesta"
-      ><img class="mw1" src="../static/github.svg"
-    /></a>
+      class="link white loud-yellow h2 w2 mr3"
+      href="https://github.com/kelvinscuesta"
+      title="GitHub"
+    >
+      <svg
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        stroke-linejoin="round"
+        stroke-miterlimit="1.414"
+      >
+        <path
+          d="M8 0C3.58 0 0 3.582 0 8c0 3.535 2.292 6.533 5.47 7.59.4.075.547-.172.547-.385 0-.19-.007-.693-.01-1.36-2.226.483-2.695-1.073-2.695-1.073-.364-.924-.89-1.17-.89-1.17-.725-.496.056-.486.056-.486.803.056 1.225.824 1.225.824.714 1.223 1.873.87 2.33.665.072-.517.278-.87.507-1.07-1.777-.2-3.644-.888-3.644-3.953 0-.873.31-1.587.823-2.147-.083-.202-.358-1.015.077-2.117 0 0 .672-.215 2.2.82.638-.178 1.323-.266 2.003-.27.68.004 1.364.092 2.003.27 1.527-1.035 2.198-.82 2.198-.82.437 1.102.163 1.915.08 2.117.513.56.823 1.274.823 2.147 0 3.073-1.87 3.75-3.653 3.947.287.246.543.735.543 1.48 0 1.07-.01 1.933-.01 2.195 0 .215.144.463.55.385C13.71 14.53 16 11.534 16 8c0-4.418-3.582-8-8-8"
+        />
+      </svg>
+    </a>
+    <a
+      class="link loud-yellow white h2 w2 mr3"
+      href="https://www.linkedin.com/in/kelvinscuesta/"
+    >
+      <svg
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 16 16"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        stroke-linejoin="round"
+        stroke-miterlimit="1.414"
+      >
+        <path
+          d="M13.632 13.635h-2.37V9.922c0-.886-.018-2.025-1.234-2.025-1.235 0-1.424.964-1.424 1.96v3.778h-2.37V6H8.51V7.04h.03c.318-.6 1.092-1.233 2.247-1.233 2.4 0 2.845 1.58 2.845 3.637v4.188zM3.558 4.955c-.762 0-1.376-.617-1.376-1.377 0-.758.614-1.375 1.376-1.375.76 0 1.376.617 1.376 1.375 0 .76-.617 1.377-1.376 1.377zm1.188 8.68H2.37V6h2.376v7.635zM14.816 0H1.18C.528 0 0 .516 0 1.153v13.694C0 15.484.528 16 1.18 16h13.635c.652 0 1.185-.516 1.185-1.153V1.153C16 .516 15.467 0 14.815 0z"
+          fill-rule="nonzero"
+        />
+      </svg>
+    </a>
+    <a
+      class="link loud-yellow white h2 w2 mr3"
+      href="https://www.linkedin.com/in/kelvinscuesta/"
+    >
+      <svg
+        version="1.1"
+        fill="currentColor"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="0 0 60 60"
+        xml:space="preserve"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        stroke-linejoin="round"
+        stroke-miterlimit="1.414"
+      >
+        <g>
+          <g>
+            <g>
+              <path
+                d="M2,2h39v7h2V1c0-0.6-0.4-1-1-1H1C0.4,0,0,0.4,0,1v58c0,0.6,0.4,1,1,1h32v-2H2V2z"
+              />
+              <path
+                d="M43.7,21.3l-2.8-6.6c-0.1-0.4-0.5-0.6-0.9-0.6s-0.7,0.2-0.9,0.6l-2.8,6.6C36.1,21.5,36,21.7,36,22v31v6c0,0.6,0.4,1,1,1
+				h6c0.6,0,1-0.4,1-1v-6V22C44,21.7,43.9,21.4,43.7,21.3z M38,23h4v29h-4V23z M40,17.5l1.5,3.5h-3L40,17.5z M42,58h-4v-4h4V58z"
+              />
+              <path
+                d="M59,38H48c-0.6,0-1,0.4-1,1v20c0,0.6,0.4,1,1,1h11c0.6,0,1-0.4,1-1V39C60,38.4,59.6,38,59,38z M58,40v5h-9v-5H58z M49,58
+				V47h9v11H49z"
+              />
+              <path
+                d="M27,11c0-3.3-2.7-6-6-6s-6,2.7-6,6s2.7,6,6,6S27,14.3,27,11z M17,11c0-2.2,1.8-4,4-4s4,1.8,4,4s-1.8,4-4,4S17,13.2,17,11
+				z"
+              />
+              <rect x="15" y="20" width="12" height="2" />
+              <rect x="15" y="25" width="12" height="2" />
+              <rect x="6" y="31" width="15" height="2" />
+              <rect x="6" y="36" width="26" height="2" />
+              <rect x="6" y="41" width="26" height="2" />
+              <rect x="6" y="46" width="8" height="2" />
+              <rect x="6" y="51" width="11" height="2" />
+              <rect x="21" y="46" width="8" height="2" />
+              <rect x="21" y="51" width="11" height="2" />
+            </g>
+          </g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+          <g></g>
+        </g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+      </svg>
+    </a>
   </div>
 </template>
 
@@ -17,4 +123,8 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.justify-center {
+  justify-content: center;
+}
+</style>
